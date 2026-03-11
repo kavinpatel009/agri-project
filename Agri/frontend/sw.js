d5f0fafdd1c70f166efb364b0d41c027f434fd2a
kavinpatel009 <kavinjpatel104@gmail.com>
2026-03-11 10:15:53 +0530

    Delete Agri/frontend directory

--- Agri/frontend/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'agriverse-v2';
-const FILES = ['/', '/index.html', '/style.css', '/script.js', '/images/logoss.jpg'];
-
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
