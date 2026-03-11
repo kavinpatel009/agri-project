19b75a40d0af2f573359b4e7331b5ecae47180b9
kavinpatel009 <kavinjpatel104@gmail.com>
2026-03-11 16:58:50 +0530

    Refactor cache deletion logic in service worker

--- Agri/frontend/sw.js
+++ Agri/frontend/sw.js
@@ -9,10 +9,7 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => {
-        console.log('🗑️ Deleting old cache:', k);
-        return caches.delete(k);
-      }))
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     ).then(() => clients.claim())
   );
 });
