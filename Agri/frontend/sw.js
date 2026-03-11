91c3bd990216597e4dd0e4f3a088bab0b5fdc47a
kavinpatel009 <kavinjpatel104@gmail.com>
2026-03-11 11:16:39 +0530

    Update service worker cache version to v3

--- Agri/frontend/sw.js
+++ Agri/frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agriverse-v2';
+const CACHE = 'agriverse-v3';
 const FILES = ['/', '/index.html', '/style.css', '/script.js', '/images/logoss.jpg'];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
